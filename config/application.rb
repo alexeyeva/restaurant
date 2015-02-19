2ca44b8d56e22802a0db428000d173e1059c4843
Kateryna Alexeyeva <kateryna.alexeyeva@gmail.com>
2015-02-19 14:51:23 +0200

    sass -> coffee

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,8 @@ module Restaraunt
     config.assets.initialize_on_precompile=false
     
     config.assets.precompile += %w( bootstrap.css )
+    
+    config.serve_static_assets = true
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,10 @@ Restaraunt::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
+  config.cache_classes = true
+  config.serve_static_assets = true
   config.assets.compile = true
+  config.assets.digest = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
