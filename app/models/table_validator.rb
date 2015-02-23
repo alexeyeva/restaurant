0ebeb4dce32e16ecfc6030cdb552c593253a1eb6
Kate Alexeyeva <kateryna.alexeyeva@gmail.com>
2015-02-23 16:10:10 +0200

    tests and validator

--- app/models/table_validator.rb
+++ app/models/table_validator.rb
@@ -1,5 +1,6 @@
 class TableValidator < ActiveModel::Validator
   
+  
   def validate(record)
     if record.table_number.nil? || record.start_time.nil? || record.end_time.nil?
       record.errors[:base] << "fields can't be blank"
@@ -7,22 +8,27 @@ class TableValidator < ActiveModel::Validator
       if record.start_time >= record.end_time
         record.errors[:base] << "invalid reservation time"
       else
-        reservations = Reservation.where("table_number = ?", record.table_number).all
-        if reservations.present?
-          reservations.each do |r|
-            if record.start_time >= r.start_time && record.start_time <= r.end_time
-              record.errors[:base] << "this table is reserved"
-            elsif record.end_time <= r.end_time && record.end_time >= r.start_time
-              record.errors[:base] << "this table is reserved"
-            elsif r.start_time >= record.start_time && r.start_time <= record.end_time
-              record.errors[:base] << "this table is reserved"
-            end
-          end
-        end
+        reserved_table(record)
       end
     end
   end
   
+  private
+
+  def reserved_table(record)
+    
+    reservations = Reservation.where("table_number = ?", record.table_number).all
+    if reservations.present?
+      reservations.each do |r|
+        case
+        when record.start_time >= r.start_time && record.start_time <= r.end_time then record.errors[:base] << "this table is reserved"
+        when record.end_time <= r.end_time && record.end_time >= r.start_time then record.errors[:base] << "this table is reserved"
+        when r.start_time >= record.start_time && r.start_time <= record.end_time then record.errors[:base] << "this table is reserved"
+        end
+      end
+    end
+    
+  end
   
   
 end
--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -5,75 +5,59 @@ describe Reservation do
   describe ".create" do
     
     context "when valid" do
-      let(:reserv) { FactoryGirl.build(:reservation, table_number: 2, start_time: "15-02-18 15:00:00", end_time: "15-02-18 16:00:00") }
-      let(:reserv1) { FactoryGirl.build(:reservation, table_number: 2, start_time: "15-02-18 17:00:00", end_time: "15-02-18 18:00:00") }
+      let(:reserv) { FactoryGirl.create(:reservation, table_number: 2, start_time: "15-02-18 15:00:00", end_time: "15-02-18 16:00:00") }
+      let(:reserv1) { FactoryGirl.create(:reservation, table_number: 2, start_time: "15-02-18 17:00:00", end_time: "15-02-18 18:00:00") }
       it "should create" do
-        reserv.should be_valid
-        reserv.save
-        reserv1.should be_valid
+        expect {
+          reserv
+          reserv1
+        }.to change{Reservation.count}.by(2)
       end
     end
     
     context "when invalid" do
-
-      describe "with the same reservation time" do
-        let(:reserv) { FactoryGirl.build(:reservation, table_number: 2, start_time: "15-02-18 15:00:00", end_time: "15-02-18 16:00:00") }
-        let(:reserv1) { FactoryGirl.build(:reservation, table_number: 2, start_time: "15-02-18 15:00:00", end_time: "15-02-18 16:00:00") }
+      
+      shared_examples "invalid record" do
         it "shouldn't create" do
-          reserv.should be_valid
-          reserv.save
+          reserv
           reserv1.should_not be_valid
         end
+      end
+
+      describe "with the same reservation time" do
+        let(:reserv) { FactoryGirl.create(:reservation, table_number: 2, start_time: "15-02-18 15:00:00", end_time: "15-02-18 16:00:00") }
+        let(:reserv1) { FactoryGirl.build(:reservation, table_number: 2, start_time: "15-02-18 15:00:00", end_time: "15-02-18 16:00:00") }
+        it_behaves_like "invalid record"
       end
       
       describe "when new reservation during old one" do
-        let(:reserv) { FactoryGirl.build(:reservation, table_number: 2, start_time: "15-02-18 15:00:00", end_time: "15-02-18 17:00:00") }
+        let(:reserv) { FactoryGirl.create(:reservation, table_number: 2, start_time: "15-02-18 15:00:00", end_time: "15-02-18 17:00:00") }
         let(:reserv1) { FactoryGirl.build(:reservation, table_number: 2, start_time: "15-02-18 16:00:00", end_time: "15-02-18 16:55:00") }
-        it "shouldn't create" do
-          reserv.should be_valid
-          reserv.save
-          reserv1.should_not be_valid
-        end
+        it_behaves_like "invalid record"
       end
       
       describe "when new reservation starts before and ends during old one" do
-        let(:reserv) { FactoryGirl.build(:reservation, table_number: 2, start_time: "15-02-17 15:00:00", end_time: "15-02-17 16:00:00") }
+        let(:reserv) { FactoryGirl.create(:reservation, table_number: 2, start_time: "15-02-17 15:00:00", end_time: "15-02-17 16:00:00") }
         let(:reserv1) { FactoryGirl.build(:reservation, table_number: 2, start_time: "15-02-17 14:00:00", end_time: "15-02-17 15:30:00") }
-        it "shouldn't create" do
-          reserv.should be_valid
-          reserv.save
-          reserv1.should_not be_valid
-        end
+        it_behaves_like "invalid record"
       end
       
       describe "when new reservation starts during old one" do
-        let(:reserv) { FactoryGirl.build(:reservation, table_number: 2, start_time: "15-02-17 15:00:00", end_time: "15-02-17 16:00:00") }
+        let(:reserv) { FactoryGirl.create(:reservation, table_number: 2, start_time: "15-02-17 15:00:00", end_time: "15-02-17 16:00:00") }
         let(:reserv1) { FactoryGirl.build(:reservation, table_number: 2, start_time: "15-02-17 15:30:00", end_time: "15-02-17 16:30:00") }
-        it "shouldn't create" do
-          reserv.should be_valid
-          reserv.save
-          reserv1.should_not be_valid
-        end
+        it_behaves_like "invalid record"
       end
       
       describe "when new reservation ends during old one" do
-        let(:reserv) { FactoryGirl.build(:reservation, table_number: 2, start_time: "15-02-17 15:00:00", end_time: "15-02-17 16:00:00") }
+        let(:reserv) { FactoryGirl.create(:reservation, table_number: 2, start_time: "15-02-17 15:00:00", end_time: "15-02-17 16:00:00") }
         let(:reserv1) { FactoryGirl.build(:reservation, table_number: 2, start_time: "15-02-17 14:30:00", end_time: "15-02-17 15:30:00") }
-        it "shouldn't create" do
-          reserv.should be_valid
-          reserv.save
-          reserv1.should_not be_valid
-        end
+        it_behaves_like "invalid record"
       end
       
       describe "when old reservation during new one" do
-        let(:reserv) { FactoryGirl.build(:reservation, table_number: 2, start_time: "15-02-18 15:00:00", end_time: "15-02-18 16:00:00") }
+        let(:reserv) { FactoryGirl.create(:reservation, table_number: 2, start_time: "15-02-18 15:00:00", end_time: "15-02-18 16:00:00") }
         let(:reserv1) { FactoryGirl.build(:reservation, table_number: 2, start_time: "15-02-18 14:00:00", end_time: "15-02-18 17:00:00") }
-        it "shouldn't create" do
-          reserv.should be_valid
-          reserv.save
-          reserv1.should_not be_valid
-        end
+        it_behaves_like "invalid record"
       end
       
     end
@@ -83,31 +67,23 @@ describe Reservation do
   describe ".update" do
     
     context "when valid" do
-      let(:reserv) { FactoryGirl.build(:reservation, table_number: 2, start_time: "15-02-18 15:00:00", end_time: "15-02-18 16:00:00") }
-      let(:reserv1) { FactoryGirl.build(:reservation, table_number: 2, start_time: "15-02-18 16:30:00", end_time: "15-02-18 16:55:00") }
+      let(:reserv) { FactoryGirl.create(:reservation, table_number: 2, start_time: "15-02-18 15:00:00", end_time: "15-02-18 16:00:00") }
+      let(:reserv1) { FactoryGirl.create(:reservation, table_number: 2, start_time: "15-02-18 16:30:00", end_time: "15-02-18 16:55:00") }
       let(:update) { {table_number: 2, start_time: "15-02-19 17:00:00", end_time: "15-02-19 18:00:00"} }
       it "should update" do
-        reserv.should be_valid
-        reserv.save!
-        reserv.reload
-        reserv1.should be_valid
-        reserv1.save!
-        reserv1.reload
+        reserv
+        reserv1
         reserv.update_attributes!(update)
       end
     end
     
     context "when invalid" do
-      let(:reserv) { FactoryGirl.build(:reservation, table_number: 2, start_time: "15-02-18 15:00:00", end_time: "15-02-18 16:00:00") }
-      let(:reserv1) { FactoryGirl.build(:reservation, table_number: 2, start_time: "15-02-18 16:30:00", end_time: "15-02-18 16:55:00") }
+      let(:reserv) { FactoryGirl.create(:reservation, table_number: 2, start_time: "15-02-18 15:00:00", end_time: "15-02-18 16:00:00") }
+      let(:reserv1) { FactoryGirl.create(:reservation, table_number: 2, start_time: "15-02-18 16:30:00", end_time: "15-02-18 16:55:00") }
       let(:update) { {table_number: 2, start_time: "15-02-18 16:00:00", end_time: "15-02-18 18:00:00"} }
       it "shouldn't update" do
-        reserv.should be_valid
-        reserv.save!
-        reserv.reload
-        reserv1.should be_valid
-        reserv1.save!
-        reserv1.reload
+        reserv
+        reserv1
         reserv.update_attributes(update)
         reserv.should_not be_valid
       end
